ba0c073213a332075c924290926ba49b2f987d82
Jochen Scheib <jochen.scheib2@boschrexroth.de>
2021-11-04 11:47:15 +0000

    chg: Initial release of rest-api-description

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,4 @@
+The following is a list of contributors to the [rest-api-description](https://github.com/boschrexroth/rest-api-description) repository,
+in alphabetical order:
+
+* [Jochen Scheib](https://github.com/mapero), ![Repo Maintainer](https://badgen.net/badge/repo/maintainer/blue "Repo Maintainer")
---
+++ app/specs.js
@@ -0,0 +1,79 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2020-2021 Bosch Rexroth AG
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+let specs = [
+  {
+    "name": "Backup API v1.2",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/backup/backup.v1.2.openapi.json"
+  },
+  {
+    "name": "Bosch Rexroth Certificate Service v2.1",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/certificate-manager/certificate-manager.v2.1.openapi.json"
+  },
+  {
+    "name": "Bosch Rexroth Firewall Service v1.2",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-firewall/rexroth-firewall.v1.2.openapi.json"
+  },
+  {
+    "name": "Bosch Rexroth Identity Manager Service v2.0",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/identity-manager/identity-manager.v2.0.openapi.json"
+  },
+  {
+    "name": "Bosch Rexroth Network Manager Service v1.3",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/network-manager/network-manager.v1.3.openapi.json"
+  },
+  {
+    "name": "Bosch Rexroth Package Manager Service v1.5",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/package-manager/package-manager.v1.5.openapi.json"
+  },
+  {
+    "name": "Bosch Rexroth System Services v1.3",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/system/system.v1.3.openapi.json"
+  },
+  {
+    "name": "Bosch Rexroth VPN Manager Service v1.0",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-vpnmanager/rexroth-vpnmanager.v1.0.openapi.json"
+  },
+  {
+    "name": "Compliance API Service v1.1",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/compliance/compliance.v1.1.openapi.json"
+  },
+  {
+    "name": "License Manager API v1.0",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/package-manager/package-manager.v1.0.openapi.json"
+  },
+  {
+    "name": "Rest-API description to manage (removable) storage media v1.1",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-deviceadmin/storage/storage.v1.1.openapi.json"
+  },
+  {
+    "name": "Solutions API v1.4",
+    "url": "../ctrlx-automation/ctrlx-core/rexroth-solutions/rexroth-solutions.v1.4.openapi.json"
+  },
+  {
+    "name": "ctrlX PLC Engineering API v2.0",
+    "url": "../ctrlx-automation/ctrlx-works/plc-engineering/plc-engineering.v2.0.openapi.json"
+  },
+]
+export default specs;
